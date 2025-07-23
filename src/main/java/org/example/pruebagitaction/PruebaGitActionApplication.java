20b953e1f965d0b8d75426536f807b30ba41c23a
Cristian Mata <cristianmata@develop.mx>
2025-07-23 17:57:14 -0600

    s

---
+++ src/main/java/org/example/pruebagitaction/PruebaGitActionApplication.java
@@ -0,0 +1,13 @@
+package org.example.pruebagitaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaGitActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaGitActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pruebagitaction/PruebaGitActionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pruebagitaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaGitActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
