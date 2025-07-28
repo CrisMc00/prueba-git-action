2cd4419f8bf61e8ae504ece16af7f59ca141f1dc
Cristian Mata <cristianmata@develop.mx>
2025-07-28 10:28:15 -0600

    s

--- src/test/java/org/example/pruebagitaction/PruebaGitActionApplicationTests.java
+++ src/test/java/org/example/pruebagitaction/PruebaGitActionApplicationTests.java
@@ -1,13 +1,16 @@
 package org.example.pruebagitaction;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PruebaGitActionApplicationTests {
 
     @Test
     void contextLoads() {
+
+        Assertions.assertEquals(1, 1);
+        Assertions.assertEquals(1, 1);
     }
 
 }
